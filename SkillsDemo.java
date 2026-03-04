297e7efcd36c9f398b1621fbcdb1dc7c4878deb8
Alex Sheehan <2025asheehan@loetbstudent.ie>
2026-03-04 10:41:50 +0000

    Create SkillsDemo.java
    Blank state right now, more soon to be added

---
+++ SkillsDemo.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.skillsdemo;
+
+/**
+ * Date: 04/03/26
+ *
+ * @author CaelinWhelan, ShaneCaulfild, ThomasSharp, AlexSheehan
+ */
+public class SkillsDemo {
+
+    public static void main(String[] args) {
+        
+    }
+}
